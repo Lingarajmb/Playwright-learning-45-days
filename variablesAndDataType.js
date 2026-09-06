7e76ea9a0808cf8281158092d8e90adc19918860
Lingaraj <lingarajmb1997@gmail.com>
2026-09-06 18:25:50 +0530

    Day 01: JavaScript introduction, first program and variables

--- variablesAndDataType.js
+++ variablesAndDataType.js
@@ -1,4 +1,47 @@
 
+//variables and data types in javascript
+
+//Var variable
 var name = 'Lingaraj'; // String
-//document.getElementById('name').innerHTML = name;
 console.log(name);
+
+//let  variable 
+//you can use like this method1 to declare a variable in javascript
+// let candidateName = 'Lingaraj Belagali'; // String
+// console.log(candidateName);
+
+// // method2  to declare a variable in javascript
+let candidateName = 'Lingaraj Belagali'; // String
+candidateName = 'Lingar'; // String
+console.log(candidateName);
+
+//Const variable
+const Name = 'Lingaraj Mahanatesh Belagali'; // String
+console.log(Name);
+
+
+//------------------
+//Data types in javascript
+
+//String
+let firstName = 'Lingaraj';
+console.log(firstName);
+
+//Number
+let age = 25; // Number
+console.log(age);
+
+let salary = 25000.50; // Number
+console.log(salary);
+
+//Boolean
+let isMarried = false; // Boolean
+console.log(isMarried);
+
+//Undefined
+let address; // Undefined
+console.log(address);
+
+//Null
+let city = null; // Null
+console.log(city);  
